cb7ba1dc4679deb235834bfea15c38baf1391588
Idir Ammani <idirammani@MacBook-Pro-de-Idir-2.local>
2026-02-10 00:53:27 +0100

    conf + bdd

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# pokedex
+# todolist
---
+++ ui/vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    host: '0.0.0.0',  // Écoute sur toutes les interfaces réseau
+    port: 5173,       // Port par défaut de Vite
+    strictPort: true, // Échoue si le port est déjà utilisé
+    watch: {
+      usePolling: true,  // Force Vite à utiliser le polling pour détecter les changements
+    },
+  },
+})
